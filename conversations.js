65b6eae3a3558c2409082347fce8adab78c105c5
RobinRees <Reehees12@gmail.com>
2026-05-05 15:10:51 +0200

    Create conversations.js

---
+++ conversations.js
@@ -0,0 +1,83 @@
+const dialogIngrid = [
+  { name: "Leopold", line: "Ursäkta mig frun?" },
+
+  { name: "Ingrid", line: "Ska du också hekla och ha dig, jag har ju redan sagt att det inte var jag!" },
+
+  { name: "Leopold", line: "..." },
+
+  { name: "Ingrid", line: "Jaha, du är herrn som dom sa kan hjälpa mig! Låt mig berätta från början..." },
+
+  { name: "Ingrid", line: "För två dagar sedan var det någon som förgiftade vattnet här i Pildammen. Jag tvättade byfolkets kläder som vilken annan dag, sen plötsligt kom det två konstaplar och anklagade mig för dessa förfärligheter." },
+
+  { name: "Ingrid", line: "Jag förklarade för dem att det är omöjligt att det skulle vara jag, då jag dagen innan hade kurerat min sjuka son. De gav sig inte, och antar att det måste vara jag för att jag arbetar vid vattnet. Men jag tror det är på grund av att jag inte räknas som fint folk i denna stad." },
+
+  { name: "Leopold", line: "Så du var hemma kvällen när brottet begicks?" },
+
+  { name: "Ingrid", line: "Ja, du förstår – min son har varit sjuk under en längre tid. Han lider av en förfärlig hosta som inte ger sig. Just den kvällen var hans feber högre än vanligt och jag behövde sitta vid hans sängkant och ha koll på hans temperatur vart tionde minut." },
+
+  { type: "info", line: "Hm, denna informationen låter värd att anteckna inför detta mysterium." },
+
+  { name: "Ingrid", line: "Så ingen kan säga om jag talar sanning eller inte, men JAG vet sanningen. Jag hade inget med det att göra! Jag tycker du ska ta ett snack med Gustaf Larsson..." },
+
+  { type: "info", line: "Ta upp kartan" }
+];
+
+const dialogGustaf = [
+  { name: "Leopold", line: "Goddag, är det du som är Gustaf? Jag försöker förstå vad som hände med vattnet i dammen." },
+
+  { name: "Gustaf", line: "Det stämmer. Jag ansvarar för underhållet av ledningarna här i området..." },
+
+  { name: "Gustaf", line: "Jag var här hela kvällen. Arbetade med systemet. Det är mitt ansvar att se till att det fungerar, inte att förstöra det." },
+
+  { name: "Gustaf", line: "Det är skönt att de hittade den skyldige så snabbt, man kan ju aldrig lita fullt ut på tjänstefolket, inte sant?" },
+
+  { name: "Leopold", line: "Vad får dig att tro att det är tjänstefolk som är de skyldiga?" },
+
+  { name: "Gustaf", line: "Jag säger inte att det måste vara så… men man får se till omständigheterna..." },
+
+  { name: "Leopold", line: "Tack för din tid. Jag beger mig vidare nu." }
+];
+
+const dialogAugust = [
+  { name: "August", line: "Ursäkta, herrn? Jag har sett att du rör dig omkring här..." },
+
+  { name: "Leopold", line: "Jag är här på uppdrag angående brottet..." },
+
+  { name: "August", line: "August Strömbäck var namnet! Just ja, det förgiftade vattnet..." },
+
+  { name: "August", line: "Jag är rädd att jag inte kan vara till så mycket hjälp..." },
+
+  { name: "Leopold", line: "Ni verkar lugn inför något som satt hela staden i oro." },
+
+  { name: "August", line: "Man får försöka hålla huvudet kallt…" },
+
+  { name: "Leopold", line: "Ni säger att ni inte var i staden…" },
+
+  { name: "August", line: "Men om ni verkligen vill förstå vad som kan ha hamnat i vattnet… då kanske ni borde tala med apotekaren. Eller hans dotter – Doris." },
+
+  { name: "Leopold", line: "Tack för er tid. Jag går vidare till apoteket." }
+];
+
+const dialogDoris = [
+  { name: "Leopold", line: "Goddag, är du Doris Lejon?" },
+
+  { name: "Doris", line: "Ehm... Ja, det är mitt namn..." },
+
+  { name: "Leopold", line: "Jag undersöker förgiftningen av Pildammens vatten..." },
+
+  { name: "Doris", line: "Jo, hela staden talar om händelsen..." },
+
+  { name: "Leopold", line: "Får jag fråga vart fröken befann sig?" },
+
+  { name: "Doris", line: "Tror du att det kan vara jag?! Jag arbetar med att hjälpa..." },
+
+  { name: "Leopold", line: "Du nämnde Ingrid..." },
+
+  { name: "Doris", line: "Ja... jag har gett henne mediciner vid sidan av..." },
+
+  { name: "Leopold", line: "Förståeligt. Du har ett stort hjärta." },
+
+  { name: "Doris", line: "Gud nej! Tåget till Lund går endast en gång om dagen..." },
+
+  { name: "Leopold", line: "Okej, intressant. Tack för din tid." }
+];
